4d92a36b36893705cd95e5bfff5a62b992bec183
CiaranMacSrabhog <55541767+CiaranMacSrabhog@users.noreply.github.com>
2019-10-14 17:46:56 +0100

    Update CamelCase.cpp

--- COM326W2L1/Solution for W2/CamelCase/CamelCase.cpp
+++ COM326W2L1/Solution for W2/CamelCase/CamelCase.cpp
@@ -2,8 +2,8 @@
 * CamelCase.cpp
 *
 * Version information
-* Author: Dr. Shane Wilson & Dr. Effirul Ramlan
-* Date: 29/09/2017 -- Modified: 29/09/2019
+* Author: Ciaran Mcstravick
+* Date: 
 * Description: Solution to the CamelCase challenge
 * Copyright notice
 */
