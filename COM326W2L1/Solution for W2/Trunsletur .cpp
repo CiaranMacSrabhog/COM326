429e46215976d5e87a8a919e85f1dc69b646f669
CiaranMacSrabhog <55541767+CiaranMacSrabhog@users.noreply.github.com>
2019-10-14 17:47:30 +0100

    Update Trunsletur .cpp

--- COM326W2L1/Solution for W2/Trunsletur .cpp
+++ COM326W2L1/Solution for W2/Trunsletur .cpp
@@ -2,8 +2,8 @@
 * Trunsletur.cpp
 *
 * Version information
-* Author: Dr. Shaun Wilson & Dr. Effirul Ramlan
-* Date: 29/09/2017 -- Modified: 29/09/2019
+* Author: Ciaran Mcstravick
+* Date: 
 * Description: A quick translator program (Disclaimer: Basic rules and not comprehensive)
 * Copyright notice
 */
